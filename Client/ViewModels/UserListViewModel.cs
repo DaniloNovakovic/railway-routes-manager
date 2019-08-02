941a52c302d95423e804caa78ace3dcada952e5f
Danilo Novakovic <dakenzi97@gmail.com>
2019-08-02 16:52:58 +0200

    Implemented OnLoaded event (Fixes AB#23)

--- Client/ViewModels/UserListViewModel.cs
+++ Client/ViewModels/UserListViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
 using Client.Core;
 using Client.Helpers;
 
@@ -6,23 +9,29 @@ namespace Client.ViewModels
 {
     public class UserListViewModel : ViewModelBase
     {
-        public UserListViewModel()
+        private readonly IUserService _userService;
+
+        public UserListViewModel(IUserService userService)
         {
-            UserList = new ObservableCollection<UserModel>()
-            {
-                new UserModel()
-                {
-                    Username = "admin",
-                    Password = "admin",
-                    FirstName = "Danilo",
-                    LastName = "Novakovic",
-                    RoleName = "Administrator",
-                    Id = 1
-                }
-            };
+            UserList = new ObservableCollection<UserModel>();
+            _userService = userService;
         }
 
         public UserModel SelectedUser { get; set; }
         public ObservableCollection<UserModel> UserList { get; }
+
+        public override async Task OnLoadedAsync()
+        {
+            try
+            {
+                var users = await _userService.GetAllUsersAsync();
+                UserList.Clear();
+                UserList.AddRange(users);
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.InnerException?.Message ?? ex.Message);
+            }
+        }
     }
 }
