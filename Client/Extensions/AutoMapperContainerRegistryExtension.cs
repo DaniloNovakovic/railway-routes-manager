a7ac6430fc50897971dfa8ec1bd472e0c8951145
Danilo Novakovic <dakenzi97@gmail.com>
2019-08-08 12:41:25 +0200

    Added AutoMapper configuration (Fixes AB#33)

--- Client/Extensions/AutoMapperContainerRegistryExtension.cs
+++ Client/Extensions/AutoMapperContainerRegistryExtension.cs
@@ -9,16 +9,30 @@ namespace Client.Extensions
     {
         public static void RegisterAutoMapper(this IContainerRegistry containerRegistry)
         {
-            var config = new MapperConfiguration(cfg =>
-            {
-                cfg.CreateMap<UserModel, UserDto>(MemberList.Destination);
-                cfg.CreateMap<UserDto, UserModel>(MemberList.Source);
-            });
+            var config = new MapperConfiguration(ConfigureAutoMapper);
 
             config.AssertConfigurationIsValid();
 
             var mapper = config.CreateMapper();
             containerRegistry.RegisterInstance(mapper);
+        }
+
+        private static void ConfigureAutoMapper(IMapperConfigurationExpression cfg)
+        {
+            cfg.CreateMap<UserModel, UserDto>(MemberList.Destination);
+            cfg.CreateMap<UserDto, UserModel>(MemberList.Source);
+
+            cfg.CreateMap<CountryModel, CountryDto>(MemberList.Destination);
+            cfg.CreateMap<CountryDto, CountryModel>(MemberList.Source);
+
+            cfg.CreateMap<LocationModel, LocationDto>(MemberList.Destination);
+            cfg.CreateMap<LocationDto, LocationModel>(MemberList.Source);
+
+            cfg.CreateMap<RailwayStationModel, RailwayStationDto>(MemberList.Destination);
+            cfg.CreateMap<RailwayStationDto, RailwayStationModel>(MemberList.Source);
+
+            cfg.CreateMap<RouteModel, RouteDto>(MemberList.Destination);
+            cfg.CreateMap<RouteDto, RouteModel>(MemberList.Source);
         }
     }
 }
