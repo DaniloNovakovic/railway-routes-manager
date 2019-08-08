c6c7eea0528d0207537e1b52ea2d8257be9bf9d9
Danilo Novakovic <dakenzi97@gmail.com>
2019-08-08 11:17:29 +0200

    Implemented GetAll method in RouteService [Back End] (Fixes AB#29)

---
+++ Server/Services/RouteService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using Common;
+using Server.Core;
+
+namespace Server.Services
+{
+    public class RouteService : IRouteService
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public RouteService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public void Add(RouteDto entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public RouteDto Get(int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<RouteDto> GetAll()
+        {
+            var routes = _unitOfWork.Routes.GetAll();
+            return routes.Select(route => _mapper.Map<RouteDto>(route));
+        }
+
+        public void Remove(int key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int key, RouteDto entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
