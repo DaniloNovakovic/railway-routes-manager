377d15c8e177059cc73dd0432cda5316e20ef8ab
Danilo Novakovic <dakenzi97@gmail.com>
2019-08-08 13:42:00 +0200

    Binded DataGrid, OnLoaded event, and Refresh button to ViewModel (Fixes AB#34)

--- Client/ViewModels/RailwayListViewModel.cs
+++ Client/ViewModels/RailwayListViewModel.cs
@@ -1,16 +1,54 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Input;
 using Client.Core;
-using Prism.Mvvm;
+using Client.Helpers;
+using Prism.Commands;
 
 namespace Client.ViewModels
 {
-    public class RailwayListViewModel : BindableBase
+    public class RailwayListViewModel : ViewModelBase
     {
+        private readonly IRouteService _routeService;
+
+        public RailwayListViewModel(IRouteService routeService)
+        {
+            _routeService = routeService;
+
+            Routes = new ObservableCollection<RouteModel>();
+            RefreshCommand = new DelegateCommand(async () => await RefreshRoutesAsync());
+        }
+
+        public ICommand RefreshCommand { get; }
         public ObservableCollection<RouteModel> Routes { get; set; }
 
-        public RailwayListViewModel()
+        public override Task OnLoadedAsync()
         {
-            Routes = new ObservableCollection<RouteModel>();
+            return RefreshRoutesAsync();
+        }
+
+        public Task RefreshRoutesAsync()
+        {
+            return SafeExecuteAsync(async () =>
+            {
+                var routes = await _routeService.GetAllRoutesAsync();
+                Routes.Clear();
+                Routes.AddRange(routes);
+            });
+        }
+
+        private async Task SafeExecuteAsync(Func<Task> callback)
+        {
+            try
+            {
+                await callback?.Invoke();
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError(ex.InnerException?.Message ?? ex.Message);
+            }
         }
     }
 }
