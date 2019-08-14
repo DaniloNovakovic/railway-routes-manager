b1eb497cdee527ad27f95fc4c31be18c30d2943e
Danilo Novakovic <dakenzi97@gmail.com>
2019-08-14 11:47:33 +0200

    Implemented AddRouteFormViewModel (Fixes AB#40)

--- Client/ViewModels/Forms/AddRouteFormViewModel.cs
+++ Client/ViewModels/Forms/AddRouteFormViewModel.cs
@@ -1,24 +1,61 @@
 ﻿using System.Collections.ObjectModel;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using Client.Core;
 using Client.Helpers;
+using Prism.Commands;
+using Prism.Regions;
 
 namespace Client.ViewModels
 {
     public class AddRouteFormViewModel : ViewModelBase
     {
+        private readonly IRegionManager _regionManager;
         private readonly IRouteService _routeService;
         private readonly IRailwayStationService _stationService;
+        private bool _canAdd;
+        private RouteModel _routeModel;
 
-        public AddRouteFormViewModel(IRouteService routeService, IRailwayStationService stationService)
+        public AddRouteFormViewModel(IRouteService routeService, IRailwayStationService stationService, IRegionManager regionManager)
         {
             _routeService = routeService;
             _stationService = stationService;
+            _regionManager = regionManager;
 
+            RouteModel = new RouteModel();
             RailwayStations = new ObservableCollection<RailwayStationModel>();
+            AddCommand = new DelegateCommand(async () => await AddRouteAsync());
+
+            RouteModel.ErrorsChanged += RouteModel_ErrorsChanged;
+        }
+
+        public ICommand AddCommand { get; set; }
+
+        public bool CanAdd
+        {
+            get { return _canAdd; }
+            set { SetProperty(ref _canAdd, value); }
         }
 
         public ObservableCollection<RailwayStationModel> RailwayStations { get; set; }
+
+        public RouteModel RouteModel
+        {
+            get { return _routeModel; }
+            set { SetProperty(ref _routeModel, value); }
+        }
+
+        public Task AddRouteAsync()
+        {
+            return SafeExecuteAsync(
+                @try: async () =>
+                {
+                    CanAdd = false;
+                    await _routeService.AddRouteAsync(RouteModel);
+                    OnRouteAdded();
+                },
+                @finally: () => CanAdd = !RouteModel.HasErrors);
+        }
 
         public override Task OnLoadedAsync()
         {
@@ -28,6 +65,16 @@ namespace Client.ViewModels
                 RailwayStations.Clear();
                 RailwayStations.AddRange(stations);
             });
+        }
+
+        private void OnRouteAdded()
+        {
+            _regionManager.RequestNavigate(RegionNames.AuthContentRegion, NavigationPaths.RailwayListPath);
+        }
+
+        private void RouteModel_ErrorsChanged(object sender, System.ComponentModel.DataErrorsChangedEventArgs e)
+        {
+            CanAdd = !RouteModel.HasErrors;
         }
     }
 }
