aceec000ec98876981186a2163b421b3619a2c0c
Danilo Novakovic <dakenzi97@gmail.com>
2019-08-15 15:34:55 +0200

    Created EditRouteCommand (Fixes AB#58)

---
+++ Client.Core/Commands/EditRouteCommand.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+
+namespace Client.Core.Commands
+{
+    public class EditRouteCommand : IUndoableCommand
+    {
+        private readonly RouteModel _newRoute;
+        private readonly IRouteService _routeService;
+        private RouteModel _oldRoute;
+
+        public EditRouteCommand(IRouteService routeService, RouteModel newRoute)
+        {
+            _routeService = routeService;
+            _newRoute = newRoute;
+        }
+
+        public async Task ExecuteAsync()
+        {
+            _oldRoute = await _routeService.GetRouteAsync(_newRoute.Id);
+            await _routeService.UpdateRouteAsync(_newRoute);
+        }
+
+        public Task UnExecuteAsync()
+        {
+            return _routeService.UpdateRouteAsync(_oldRoute);
+        }
+    }
+}
