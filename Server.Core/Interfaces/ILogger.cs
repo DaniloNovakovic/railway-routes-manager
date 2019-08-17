cdbbe65965c22d19a5ee51313e0ec8501674f49d
Danilo Novakovic <dakenzi97@gmail.com>
2019-08-17 11:16:23 +0200

    Reorganized files and folders in application to be more easily navigable

---
+++ Server.Core/Interfaces/ILogger.cs
@@ -0,0 +1,13 @@
+﻿namespace Server.Core
+{
+    public interface ILogger
+    {
+        void Debug(string message);
+
+        void Exception(string message);
+
+        void Info(string message);
+
+        void Warn(string message);
+    }
+}
