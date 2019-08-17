546e07da8c1a769b6259f7f9317d551fe808efc9
Danilo Novakovic <dakenzi97@gmail.com>
2019-08-17 15:58:38 +0200

    Created simplified LogModel class

---
+++ Client.Core/Models/LogModel.cs
@@ -0,0 +1,31 @@
+﻿using Prism.Validation;
+
+namespace Client.Core
+{
+    public class LogModel : ValidatableBindableBase
+    {
+        private string _date;
+
+        private string _level;
+
+        private string _message;
+
+        public string Date
+        {
+            get { return _date; }
+            set { SetProperty(ref _date, value); }
+        }
+
+        public string Level
+        {
+            get { return _level; }
+            set { SetProperty(ref _level, value); }
+        }
+
+        public string Message
+        {
+            get { return _message; }
+            set { SetProperty(ref _message, value); }
+        }
+    }
+}
