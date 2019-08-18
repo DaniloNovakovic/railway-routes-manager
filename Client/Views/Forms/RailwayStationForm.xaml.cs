7162bd25a37a040974e1cdbb04d8612a8324d2f9
Danilo Novakovic <dakenzi97@gmail.com>
2019-08-18 12:45:46 +0200

    Created basic RailwayStation Form View (Fixes AB#83)

---
+++ Client/Views/Forms/RailwayStationForm.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Client.Views
+{
+    /// <summary>
+    /// Interaction logic for RailwayStationForm.xaml
+    /// </summary>
+    public partial class RailwayStationForm : UserControl
+    {
+        public RailwayStationForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
