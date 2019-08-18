179f05272c9003cc3c86070fdef9e9bed8f096e1
Danilo Novakovic <dakenzi97@gmail.com>
2019-08-18 20:59:31 +0200

    Implemented all methods in RailwayPlatformService in Client (Fixes AB#94)

--- Client.Core/Interfaces/Services/IRailwayPlatformService.cs
+++ Client.Core/Interfaces/Services/IRailwayPlatformService.cs
@@ -5,6 +5,14 @@ namespace Client.Core
 {
     public interface IRailwayPlatformService
     {
+        Task<int> AddPlatformAsync(RailwayPlatformModel platform);
+
         Task<IEnumerable<RailwayPlatformModel>> GetAllPlatformsAsync();
+
+        Task<RailwayPlatformModel> GetPlatformAsync(int key);
+
+        Task RemovePlatformAsync(int key);
+
+        Task UpdatePlatformAsync(RailwayPlatformModel platform);
     }
 }
--- Client.Infrastructure/Services/RailwayPlatformService.cs
+++ Client.Infrastructure/Services/RailwayPlatformService.cs
@@ -18,6 +18,16 @@ namespace Client.Infrastructure
             _mapper = mapper;
         }
 
+        public Task<int> AddPlatformAsync(RailwayPlatformModel platform)
+        {
+            return Task.Run(() =>
+            {
+                var dto = _mapper.Map<Common.RailwayPlatformDto>(platform);
+                var proxy = GetProxy();
+                return proxy.Add(dto);
+            });
+        }
+
         public Task<IEnumerable<RailwayPlatformModel>> GetAllPlatformsAsync()
         {
             return Task.Run(() =>
@@ -25,6 +35,35 @@ namespace Client.Infrastructure
                 var proxy = GetProxy();
                 var platformDtos = proxy.GetAll();
                 return platformDtos.Select(dto => _mapper.Map<RailwayPlatformModel>(dto));
+            });
+        }
+
+        public Task<RailwayPlatformModel> GetPlatformAsync(int key)
+        {
+            return Task.Run(() =>
+            {
+                var proxy = GetProxy();
+                var dto = proxy.Get(key);
+                return _mapper.Map<RailwayPlatformModel>(dto);
+            });
+        }
+
+        public Task RemovePlatformAsync(int key)
+        {
+            return Task.Run(() =>
+            {
+                var proxy = GetProxy();
+                proxy.Remove(key);
+            });
+        }
+
+        public Task UpdatePlatformAsync(RailwayPlatformModel station)
+        {
+            return Task.Run(() =>
+            {
+                var proxy = GetProxy();
+                var dto = _mapper.Map<Common.RailwayPlatformDto>(station);
+                proxy.Update(station.Id, dto);
             });
         }
 
