83c96cd5d7f42f3c58d0d4565ecac7f9040f601b
Danilo Novakovic <dakenzi97@gmail.com>
2019-08-19 20:02:54 +0200

    Added locks to UserService (Fixes AB#106)

--- Server/Services/UserService.cs
+++ Server/Services/UserService.cs
@@ -10,6 +10,7 @@ namespace Server
     [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, ConcurrencyMode = ConcurrencyMode.Multiple)]
     public class UserService : IUserService
     {
+        private static readonly object Mutex = new object();
         private readonly ILogger _logger;
         private readonly IMapper _mapper;
         private readonly IUnitOfWork _unitOfWork;
@@ -22,6 +23,95 @@ namespace Server
         }
 
         public int Add(UserDto entity)
+        {
+            lock (Mutex)
+            {
+                return AddNew(entity);
+            }
+        }
+
+        public UserDto Get(int key)
+        {
+            User user;
+
+            lock (Mutex)
+            {
+                _logger.Debug($"Getting user {key}...");
+                user = _unitOfWork.Users.Get(key);
+            }
+
+            return _mapper.Map<UserDto>(user);
+        }
+
+        public IEnumerable<UserDto> GetAll()
+        {
+            IEnumerable<User> users;
+
+            lock (Mutex)
+            {
+                _logger.Debug($"Getting all users...");
+                users = _unitOfWork.Users.GetAll();
+            }
+
+            return users.Select(u =>
+            {
+                var dto = _mapper.Map<UserDto>(u);
+                dto.Password = "";
+                return dto;
+            }).ToList();
+        }
+
+        public UserDto GetByUsername(string username)
+        {
+            User user;
+
+            lock (Mutex)
+            {
+                _logger.Debug($"Getting user by username '{username}'...");
+                user = _unitOfWork.Users.Get(u => u.Username == username);
+            }
+
+            return _mapper.Map<UserDto>(user);
+        }
+
+        public void Remove(int key)
+        {
+            lock (Mutex)
+            {
+                _logger.Debug($"Removing user {key}...");
+
+                var user = _unitOfWork.Users.Get(key);
+                _unitOfWork.Users.Remove(user);
+                _unitOfWork.SaveChanges();
+
+                _logger.Info($"Removed user {key}!");
+            }
+        }
+
+        public void Update(int key, UserDto entity)
+        {
+            lock (Mutex)
+            {
+                _logger.Debug($"Updating user {key}...");
+
+                var user = _unitOfWork.Users.Get(key);
+
+                if (user is null)
+                {
+                    _logger.Warn($"User {key} not found!");
+                    entity.Id = key;
+                    AddNew(entity);
+                    return;
+                }
+
+                _mapper.Map(source: entity, destination: user);
+                _unitOfWork.SaveChanges();
+
+                _logger.Info($"Updated user {key}!");
+            }
+        }
+
+        private int AddNew(UserDto entity)
         {
             _logger.Debug($"Adding new user...");
 
@@ -38,57 +128,6 @@ namespace Server
             _logger.Info($"User {addedUser.Id},'{addedUser.Username}' added");
 
             return addedUser.Id;
-        }
-
-        public UserDto Get(int key)
-        {
-            _logger.Debug($"Getting user {key}...");
-
-            var user = _unitOfWork.Users.Get(key);
-            return _mapper.Map<UserDto>(user);
-        }
-
-        public IEnumerable<UserDto> GetAll()
-        {
-            _logger.Debug($"Getting all users...");
-
-            var users = _unitOfWork.Users.GetAll();
-            return users.Select(u =>
-            {
-                var dto = _mapper.Map<UserDto>(u);
-                dto.Password = "";
-                return dto;
-            }).ToList();
-        }
-
-        public UserDto GetByUsername(string username)
-        {
-            _logger.Debug($"Getting user by username '{username}'...");
-
-            var user = _unitOfWork.Users.Get(u => u.Username == username);
-            return _mapper.Map<UserDto>(user);
-        }
-
-        public void Remove(int key)
-        {
-            _logger.Debug($"Removing user {key}...");
-
-            var user = _unitOfWork.Users.Get(key);
-            _unitOfWork.Users.Remove(user);
-            _unitOfWork.SaveChanges();
-
-            _logger.Info($"Removed user {key}!");
-        }
-
-        public void Update(int key, UserDto entity)
-        {
-            _logger.Debug($"Updating user {key}...");
-
-            var user = _unitOfWork.Users.Get(key);
-            _mapper.Map(source: entity, destination: user);
-            _unitOfWork.SaveChanges();
-
-            _logger.Info($"Updated user {key}!");
         }
     }
 }
